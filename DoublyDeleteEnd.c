97904d4a35329b2ab4beb81b27c9ed85414f3ddb
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-08-27 11:35:52 +0530

    Update DoublyDeleteEnd.c

--- DoublyDeleteEnd.c
+++ DoublyDeleteEnd.c
@@ -6,6 +6,7 @@ struct Node
     struct Node*pre;
     struct Node*next;
 };
+
 void doubly_Traverse(struct Node*ptr)
 {
     while(ptr!=NULL){
@@ -18,25 +19,28 @@ struct Node*delete_At_End(struct Node *head)
 {
     struct Node*p=head;
     struct Node*q=head->next;
-    while(q->next!=NULL)
+    while(q->next!=NULL) // Traversing Upto Last
     {
         q=q->next;
         p=p->next;
     }
     p->next=NULL;
-    free(q);
+    free(q); // Deleting last node
     return head;
 };
 
 int main()
 {
-
+    // Creating nodes
+    
     struct Node*head=(struct Node*)malloc(sizeof(struct Node));
     struct Node*n2=(struct Node*)malloc(sizeof(struct Node));
     struct Node*n3=(struct Node*)malloc(sizeof(struct Node));
     struct Node*n4=(struct Node*)malloc(sizeof(struct Node));
     struct Node*n5=(struct Node*)malloc(sizeof(struct Node));
 
+    // Linking the nodes
+    
     head->pre=NULL;
     head->data=10;
     head->next=n2;
@@ -57,10 +61,13 @@ int main()
     n5->data=50;
     n5->next=NULL;
 
-    doubly_Traverse(head);
+    printf("\nBefore Deletion\n");
+    doubly_Traverse(head); // Printing before elements
 
 
-    printf("\n\nAfter Deleting Last Node\n");
+    printf("\n\nAfter Deleting Last Node\n"); 
     head=delete_At_End(head);
-    doubly_Traverse(head);
+    doubly_Traverse(head); // Printing new elements 
+
+    return 0;
 }
