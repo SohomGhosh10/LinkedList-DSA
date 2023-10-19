ae64c684abfe859b3555f8ba646d07d053ba7e50
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-10-20 00:06:33 +0530

    Add files via upload

---
+++ InOrder_Trav.C
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* CreateNode(int data){
+    struct Node* p = (struct Node*)malloc(sizeof(struct Node));
+    p->data = data;
+    p->left = NULL;
+    p->right = NULL;
+}
+
+void inOrder(struct Node* root){
+    if (root != NULL)
+    {
+        inOrder(root->left);
+        printf("Data is %d\n",root->data);
+        inOrder(root->right);
+    }
+    
+}
+
+int main(){
+    struct Node * p = CreateNode(4);
+    struct Node * p1 = CreateNode(1);
+    struct Node * p2 = CreateNode(6);
+    struct Node * p3 = CreateNode(5);
+    struct Node * p4 = CreateNode(2);
+
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+    
+    inOrder(p);
+    return 0;
+    
+}
