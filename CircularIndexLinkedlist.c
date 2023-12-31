2f87687b21776be1324416f6a08386369b291c16
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-12-31 18:14:09 +0530

    Update CircularIndexLinkedlist.c

--- CircularIndexLinkedlist.c
+++ CircularIndexLinkedlist.c
@@ -13,7 +13,7 @@ void linkedList(struct Node * head){
     do{
           printf("Elements are %d\n",ptr->data); // at first print
     ptr = ptr->next; 
-    }while(ptr!=head); // do-while loop conditionn
+    }while(ptr!=head); // do-while loop condition
 }
 
 struct Node * insertAtIndex(struct Node * head , int data ,int index){
